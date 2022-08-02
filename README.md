7ee2dc6d541c450e4250d5321107b1c1cf291a74
Stephpp <stephenpapp30@gmail.com>
2022-08-03 00:41:07 +0300

    Email form adjustments

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Personal Portfolio Website
-Portfolio Showcase Website using React, Webpack and Babel. This Website could also be made with pure HTML and CSS, but I choose to build it with React to get familiar with certain tools and workflows.
+Portfolio Showcase Website built with React, Webpack and Babel.
 ## Preview website
-You can see the current state of the website here https://stephpp.github.io/Steph/
+You can see the current state of the website here: https://stephpp.github.io
 
-Alternatively, you can download the source files or clone the repo, and then open the Website on your device by openning a terminal in the project's folder and:
+To run it locally, you can download the source files or clone the repo, and then do the following:
 
-install needed node packages: 
+- install needed node packages: 
 
+    `npm install`
 
-`npm install`
+- run the "webpack serve" script: 
 
-run the "webpack serve" script: 
-
-`npm run dev`
+    `npm run dev`
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../App.css';
-import email from '../SVG/email.svg';
 import gh from '../SVG/gh.svg';
 import ig from '../SVG/ig.svg';
 import linkedin from '../SVG/in.svg';
 import fb from '../SVG/fb.svg';
+import emailjs from 'emailjs-com';
+import { credentials } from '../EmailJs-Credentials';
+import validator from 'validator';
+
 
 const Contact = () => {
+    const contactRef = useRef();
+    const [sendText, setsendText] = useState('Send')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = document.forms["form"].name.value;
+        const subject = document.forms["form"].subject.value;
+        const email = document.forms["form"].email.value;
+        const content = document.forms["form"].content.value;
+        const message = document.getElementById('c-send-message');
+
+        if (name === "" || subject === "" || email === "" || content === "") { //empty field check
+
+                message.innerHTML = 'Please fill out all the fields'
+                message.style.color = '#ff3c00'
+                return;
+        }
+
+        if (!validator.isEmail(email)) { //invalid email check
+          
+                message.innerHTML = 'Invalid E-mail'
+                message.style.color = '#ff3c00'
+                return;
+          
+        } else {
+            emailjs.sendForm(
+                credentials.service_id,
+                credentials.template_id,
+                contactRef.current,
+                credentials.public_key
+            )
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+            setsendText('Sending...')
+            setInterval(() => {
+                message.innerHTML = 'Message Sent'
+                message.style.color = '#95D7AE'
+                setsendText('Send')
+            }, 2000);
+        }
+    }
     return (
         <div className="contact">
             <h1 className='contact-section-title'>Get in Touch</h1>
@@ -47,23 +93,16 @@ const Contact = () => {
                 <div className="c-right">
                     <div className="c-right-wrapper">
                         <h2 className='c-title'>Or send a direct email:</h2>
-                        <div className="c-form">
-                            <div>
-                                <input type="text" placeholder='Name' />
+                        <form name='form' className="c-form" ref={contactRef} onSubmit={handleSubmit}>
+                            <input type="text" placeholder='Name' name='name' autoComplete='new-name' />
+                            <input type="text" placeholder='Subject' name='subject' autoComplete='new-subject' />
+                            <input type="text" placeholder='E-mail' name='email' autoComplete='new-email' />
+                            <textarea id='content' name="content" cols="30" rows="10" placeholder='Content' />
+                            <div className='c-send-section'>
+                                <button className='c-send-button'>{sendText}</button>
+                                <h2 id='c-send-message'></h2>
                             </div>
-                            <div>
-                                <input type="text" placeholder='Subject' />
-                            </div>
-                            <div>
-                                <input type="text" placeholder='E-mail' />
-                            </div>
-                            <div>
-                                <h3 className='c-content-title'>Content</h3>
-                                <textarea name="" id="" cols="30" rows="10">
-                                </textarea>
-                            </div>
-                            <button className='c-sumbit-button'>Submit</button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
